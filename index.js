fb0e7e5b68ceee0f67a408d8e6863132f3bdaad3
GitPro10 <israfilmiya120@gmail.com>
2022-10-04 05:18:57 +0600

    Deploying in vercel

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+/*
+import validateBulk from "./api/validate-bulk.js";
+import validateOne from "./api/validate-one.js";
+*/
+import info from "./api/info.js";
+const port = 8000
+const app = express()
+app.use(express.json());
+/*
+app.post("/api/validate-bulk", validateBulk)
+app.post("/api/validate-one", validateOne)
+
+app.get("/", (req, res)=> {
+    res.sendFile('index.html', { root: '.' })
+})
+*/
+app.use("/api", info)
+app.listen(port, () => {
+    console.info(`API is up on port ${port}`);
+});
